916314566472febb76c816abb723fd5e2dc4fd81
php-rest4 <dreih796@gmail.com>
2021-06-08 10:15:40 +0300

    init RBAC

--- config/web.php
+++ config/web.php
@@ -19,6 +19,10 @@ $config = [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+        'authManager' => [
+            'class' => 'yii\rbac\PhpManager',
+            'defaultRoles' => ['admin', 'user'],
+        ],
         'user' => [
             'identityClass' => 'app\models\User',
             'enableAutoLogin' => true,
